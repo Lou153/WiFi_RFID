61ed4a07452f3f69f9e9ff8a209cb022b70bead8
Lou153 <G00305052@gmit.com>
2017-10-20 12:49:54 +0100

    First code commit and push to Github Test

---
+++ src/LouiseNiChonghaile/Main.java
@@ -0,0 +1,9 @@
+package LouiseNiChonghaile;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("First commit for git");
+    }
+}
